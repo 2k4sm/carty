46e38b2a2b3a8f43d1702c68b073f5e2b00ee68d
Shrinibas Mahanta <shrinibasmahanta2004@gmail.com>
2024-03-12 14:55:10 +0530

    initial commit.

---
+++ src/main/java/com/sm2k4/carty/CartyApplication.java
@@ -0,0 +1,13 @@
+package com.sm2k4.carty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CartyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sm2k4/carty/CartyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sm2k4.carty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
